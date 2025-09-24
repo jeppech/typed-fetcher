b83081915fcabdb2e528d71428025d8fd5778d87
Jeppe Christiansen <jeppe@codr.dk>
2025-09-24 13:57:20 +0200

    fix: incorrect exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export type { HttpResponse, HttpResponseErr, HttpResponseOk, ParseError, BaseError } from './response.js';
+export type { HttpResponse, HttpResponseErr, HttpResponseOk } from './response.js';
+export { BaseError, ParseError } from './response.js';
 export type { Endpoint, EndpointResponse, EndpointSpec, Jsonable } from './types.js';
 
 export { Fetcher, TypedFetcher } from './fetcher.js';
