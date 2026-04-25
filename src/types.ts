4222de08c77ea02636d3c6e0f2042a9134c29c5d
Jeppe Christiansen <jeppe@codr.dk>
2026-04-25 22:52:51 +0200

    feat: add endpoint definition functions

--- src/types.ts
+++ src/types.ts
@@ -27,6 +27,18 @@ export type EndpointSpec = {
   [key: string]: EndpointMethodSpec;
 };
 
+type EndpointMethodInput = Endpoint | EndpointResponse;
+
+type NormalizeEndpoint<TEndpoint extends EndpointMethodInput> = TEndpoint extends EndpointResponse
+  ? Endpoint<TEndpoint>
+  : TEndpoint;
+
+type NormalizeEndpointMethodSpec<TMethods extends Record<string, EndpointMethodInput>> = {
+  [TMethod in keyof TMethods]: NormalizeEndpoint<TMethods[TMethod]>;
+};
+
+export type EndpointBuilderSpec = Record<string, Record<string, EndpointMethodInput>>;
+
 export type EndpointsForPath<TSpec extends EndpointSpec, TPath extends keyof TSpec> = {
   [M in keyof TSpec[TPath]]: TSpec[TPath][M] extends infer R extends Endpoint ? { response: R; method: M } : void;
 }[keyof TSpec[TPath]];
@@ -46,3 +58,18 @@ export type Jsonable =
   | readonly Jsonable[]
   | { readonly [key: string]: Jsonable }
   | { toJSON(): Jsonable };
+
+export function endpoint<
+  TResponse extends EndpointResponse = EndpointResponse,
+  TRequest extends EndpointRequest = EndpointRequest,
+>(): Endpoint<TResponse, TRequest> {
+  return {} as Endpoint<TResponse, TRequest>;
+}
+
+export function define_endpoints<TSpec extends EndpointBuilderSpec>(spec: {
+  [TPath in keyof TSpec]: NormalizeEndpointMethodSpec<TSpec[TPath]>;
+}): {
+  [TPath in keyof TSpec]: NormalizeEndpointMethodSpec<TSpec[TPath]>;
+} {
+  return spec;
+}
